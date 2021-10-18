d21dbf37d3390105e7619257ffaaeb4c7a03f252
JAMorello <juan.ag.morello@gmail.com>
2021-10-18 14:05:19 -0300

    Implement routing, first version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/home">
+          {
+            // HOME
+          }
+        </Route>
+        <Route path="/search">
+          {
+            // SEARCH
+          }
+        </Route>
+        <Route path="/login">
+          {
+            // LOGIN
+          }
+        </Route>
+        <Route path="/" exact>
+          {
+            // HOME
+          }
+        </Route>
+        <Route path="*">
+          {
+            // PAGE 404
+          }
+        </Route>
+      </Switch>
+    </>
+  );
 };
 
 export default App;
