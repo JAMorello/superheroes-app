e393e8945941c523a9d311aa7aee90c0bc509cab
JAMorello <juan.ag.morello@gmail.com>
2021-10-21 00:04:20 -0300

    Add team state managmente and related fuctions

--- src/App.js
+++ src/App.js
@@ -6,21 +6,47 @@ import Home from "./components/Home";
 import Search from "./components/Search";
 
 const App = () => {
+  // State and Effect for the token -- POST request
   const [response, setResponse] = useState({ token: "", error: "" });
-
   useEffect(() => {
     localStorage.setItem("token", response.token);
   }, [response]);
+
+  // State for the team
+  const [team, setTeam] = useState([]);
+
+  const addToTeam = (heroToAdd) => {
+    setTeam([...team, heroToAdd]);
+  };
+
+  const checkExistence = (heroToAdd) => {
+    const heroID = heroToAdd.id;
+    for (let i = 0; i < team.length; i++) {
+      if (team[i].id === heroID) return true;
+    }
+    return false;
+  };
+
+  // Check if the team already has 3 members of the same alignment
+  const checkAlignment = (heroToAdd) => {
+    const alignment = heroToAdd.biography.alignment;
+    const alignmentArray = team.map((e) => e.biography.alignment === alignment);
+    return alignmentArray.length === 3;
+  };
 
   return (
     <>
       <Header token={response.token} setResponse={setResponse} />
       <Switch>
         <Route path="/home">
-          <Home />
+          <Home team={team} />
         </Route>
         <Route path="/search">
-          <Search />
+          <Search
+            addToTeam={addToTeam}
+            checkExistence={checkExistence}
+            checkAlignment={checkAlignment}
+          />
         </Route>
         <Route path="/login">
           <Login setResponse={setResponse} />
