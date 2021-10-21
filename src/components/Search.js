d56b5ecd279ee3e5975d478d8a95f058982dcbaf
JAMorello <juan.ag.morello@gmail.com>
2021-10-21 00:07:38 -0300

    Implement Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import useToken from "../customHooks/useToken";
+import SearchForm from "./Search/SearchForm";
+import HeroResults from "./Search/HeroResults";
 
-const Search = () => {
+const Search = ({ addToTeam, checkExistence, checkAlignment }) => {
   // Check if token from login exist. If not, redirect.
   useToken();
 
-  return <div></div>;
+  const [results, setResults] = useState(null);
+
+  return (
+    <>
+      <SearchForm setResults={setResults} />
+      {results && (
+        <HeroResults
+          results={results}
+          addToTeam={addToTeam}
+          checkExistence={checkExistence}
+          checkAlignment={checkAlignment}
+        />
+      )}
+    </>
+  );
 };
 
 export default Search;
