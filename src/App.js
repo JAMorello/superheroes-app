6b66d65ca558dd2d2cceb4a3c8a97d50830cd285
JAMorello <juan.ag.morello@gmail.com>
2021-10-28 19:51:37 -0300

    Lift background color

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
   const [results, setResults] = useState(null);
 
   return (
-    <div className="vh-100" style={{ backgroundColor: "#563091" }}>
+    <div style={{ backgroundColor: "#911322" }}>
       <Header
         token={response.token}
         setResponse={setResponse}
@@ -90,7 +90,7 @@ const App = () => {
             // PAGE 404
           }
         </Route>
-      </Switch>{" "}
+      </Switch>
     </div>
   );
 };
