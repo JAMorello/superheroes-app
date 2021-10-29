75b7a84d53921e3ca2ea10c8111ab6ff1039679c
JAMorello <juan.ag.morello@gmail.com>
2021-10-29 15:53:58 -0300

    Reduce code, decompose, create and implement  newcomponents

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import LoginForm from "./Login/LoginForm";
-import Logo from "../images/Logo.png";
-import superheroes from "../images/superheroes.jpg";
-
+import LoginImg from "./Login/LoginImg";
+import Logo from "./utilities/Logo";
 /*
 TODO:
 - Alerts when there is an error while login
@@ -13,18 +12,11 @@ const Login = ({ setResponse, setCurrentPage }) => {
   return (
     <div className="container-fluid w-75 bg-white mt-5 rounded shadow">
       <div className="row align-items-stretch rounded">
-        <div
-          className="col d-none d-sm-block col-md-5 col-lg-5 col-xl-6 rounded"
-          style={{
-            backgroundImage: `url(${superheroes})`,
-            backgroundPosition: "center center",
-            backgroundSize: "cover",
-          }}
-        ></div>
+        <LoginImg />
         <div className="col p-5 rounded-end">
           <div className="row">
             <div className="col d-none d-sm-block">
-              <img src={Logo} alt="Superheroes App" width="200" heigth="50" />
+              <Logo />
             </div>
             <div className="col">
               <h2 className="fw-bold text-center pt-2 pb-3">Welcome</h2>
