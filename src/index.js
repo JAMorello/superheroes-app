2e9dfc64a99fc15143b022962b3dfdc29b83752b
JAMorello <juan.ag.morello@gmail.com>
2021-10-29 00:12:35 -0300

    Update imports of Bootstrap 5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "@popperjs/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
