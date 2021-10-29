51fb244e694535e2ac0c0fe309c55b51e1205a25
JAMorello <juan.ag.morello@gmail.com>
2021-10-29 13:21:55 -0300

    Update comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,13 @@ import TeamList from "./Team/TeamList";
 import TeamStats from "./Team/TeamStats";
 import HeroDetail from "./Team/HeroDetail";
 import useToken from "../customHooks/useToken";
+import { dummyStats } from "./utilities/stats.js";
 
 const Home = ({ team, removeFromTeam, setCurrentPage }) => {
   // Check if token from login exist. If not, redirect.
   useToken();
   setCurrentPage("Home");
-  const [showDetails, setShowDetails] = useState(null);
+  const [showDetails, setShowDetails] = useState(dummyStats);
 
   return (
     <>
