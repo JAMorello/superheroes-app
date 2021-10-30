8d9a92ad825603e371229ace3ef0f4a9d7f3d252
JAMorello <juan.ag.morello@gmail.com>
2021-10-30 14:48:52 -0300

    Create new component from 'getStats' function of utilities

---
+++ src/components/Hero/HeroStats.js
@@ -0,0 +1,18 @@
+import StatIcon from "../Icons/StatIcon";
+
+const HeroStats = ({ stats, slice }) => {
+  return Object.entries(stats)
+    .slice(slice[0], slice[1])
+    .map((e) => (
+      <li class="list-group-item">
+        <StatIcon stat={e[0]} /> {e[0].charAt(0).toUpperCase() + e[0].slice(1)}:{" "}
+        {e[1]}
+      </li>
+    ));
+};
+
+HeroStats.defaultProps = {
+  slice: [0, 3],
+};
+
+export default HeroStats;
