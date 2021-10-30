a04ae204e501f6284508f6a18076bb8858bf1f6c
JAMorello <juan.ag.morello@gmail.com>
2021-10-30 14:23:12 -0300

    Change parent folder of 'HeroDetail' component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import TeamList from "./Team/TeamList";
 import TeamStats from "./Team/TeamStats";
-import HeroDetail from "./Team/HeroDetail";
+import HeroDetail from "./Hero/HeroDetail";
 import useToken from "../customHooks/useToken";
 import { dummyStats } from "./utilities/stats.js";
 
