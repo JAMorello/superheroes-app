d5a3758283bcd54975c510c56d1263f4d4fa67c2
JAMorello <juan.ag.morello@gmail.com>
2021-11-27 19:52:58 -0300

    Fix mobile responsiveness of Navbar

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../components/Icons/Logo";
 import HamburgerMenu from "../components/Header/HamburgerMenu";
 import NavLink from "../components/Header/NavLink";
 import LoginButton from "../components/Header/LoginButton";
+import "../components/Header/Navbar.css";
 
 /*
 ISSUE
@@ -10,14 +12,22 @@ ISSUE
 */
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
-        <HamburgerMenu />
+        <div onClick={() => setShowMenu(!showMenu)}>
+          <HamburgerMenu />
+        </div>
         <Link to="/home">
           <Logo />
         </Link>
-        <div className="collapse navbar-collapse" id="togglerMenu">
+        <div
+          className={`collapse navbar-collapse ${
+            showMenu ? "hidden" : "active"
+          }`}
+        >
           <ul className="navbar-nav ms-auto">
             <NavLink goToPage="Home" />
             <NavLink goToPage="Search" />
--- src/components/Header/HamburgerMenu.js
+++ src/components/Header/HamburgerMenu.js
@@ -2,15 +2,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const HamburgerMenu = () => {
   return (
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#togglerMenu"
-      aria-controls="togglerMenu"
-      aria-expanded={false}
-      aria-label="Toggle navigation"
-    >
+    <button className="navbar-toggler">
       <GiHamburgerMenu />
     </button>
   );
